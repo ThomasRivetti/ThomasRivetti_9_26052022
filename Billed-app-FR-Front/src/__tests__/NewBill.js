05d5c78a4659421477d431e4f45a8f4e9ced59fd
Rivetti <thomasrivetti87@gmail.com>
2022-06-01 22:48:31 +0200

    nettoyage

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -7,16 +7,17 @@ import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from "../__mocks__/localStorage.js"
 import mockStore from "../__mocks__/store"
-import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import userEvent from "@testing-library/user-event";
 
 jest.mock("../app/store", () => mockStore)
 
-beforeEach(()=> {
+beforeEach(() => {
   //simule la connection sur la page Employee en parametrant le localStorage
   Object.defineProperty(window, 'localStorage', { value: localStorageMock })
   window.localStorage.setItem('user', JSON.stringify({
-    type: 'Employee'
+    type: 'Employee',
+    email: 'employee@test.tld'
   }))
   //affiche la page nouvelle note de frais
   document.body.innerHTML = NewBillUI()
@@ -27,14 +28,14 @@ describe("Given I am connected as an employee", () => {
     //envoyer un formulaire plein NewBill.handleSubmit
     //justificatif doit etre conforme à l'extension jpg jepg png
     //justificatif ne doit pas être un pdf
-    describe("When I try to load file", ()=> {
-      test("Then file should be an image", ()=> {        
+    describe("When I try to load file", () => {
+      test("Then file should be an image", () => {
         //recupération input file
         const newFile = screen.getByTestId('file')
         //recupération instance de class NewBill
         const onNavigate = (pathname) => document.body.innerHTML = ROUTES({ pathname })
-        const newBillEmulation = new NewBill({document, onNavigate, store:mockStore, localStorage: window.localStorage})
-        const handleChangeFile = jest.fn((e)=> newBillEmulation.handleChangeFile(e))
+        const newBillEmulation = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+        const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
         //eventListener handleChangeFile
         newFile.addEventListener("change", handleChangeFile)
         userEvent.click(newFile)
@@ -42,19 +43,19 @@ describe("Given I am connected as an employee", () => {
         //verifie si le fichier est bien un image
         fireEvent.change(newFile, {
           target: {
-            files: [new File(['(⌐□_□)'], 'chucknorris.png', {type: 'image/png'})],
+            files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })],
           },
         })
         expect(newFile.files[0].type).toMatch(/(image\/jpg)|(image\/jpeg)|(image\/png)/gm)
       })
 
-      test("Then file should not be an image", ()=> {        
+      test("Then file should not be an image", () => {
         //recupération input file
         const newFile = screen.getByTestId('file')
         //recupération instance de class NewBill
         const onNavigate = (pathname) => document.body.innerHTML = ROUTES({ pathname })
-        const newBillEmulation = new NewBill({document, onNavigate, store:mockStore, localStorage: window.localStorage})
-        const handleChangeFile = jest.fn((e)=> newBillEmulation.handleChangeFile(e))
+        const newBillEmulation = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+        const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
         //eventListener handleChangeFile
         newFile.addEventListener("change", handleChangeFile)
         userEvent.click(newFile)
@@ -62,7 +63,7 @@ describe("Given I am connected as an employee", () => {
         //verifie si le fichier est bien un image
         fireEvent.change(newFile, {
           target: {
-            files: [new File(['(⌐□_□)'], 'chucknorris.txt', {type: 'text/plain'})],
+            files: [new File(['(⌐□_□)'], 'chucknorris.txt', { type: 'text/plain' })],
           },
         })
         expect(newFile.files[0].type).not.toMatch(/(image\/jpg)|(image\/jpeg)|(image\/png)/gm)
@@ -74,6 +75,6 @@ describe("Given I am connected as an employee", () => {
         //to-do write assertion
       })
     })
-   
+
   })
 })
